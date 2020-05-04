3a02d3687c84df668e08c2716aec527985f655d4
Abdelhadim-e <e.abdelhadim@gmail.com>
2020-05-04 19:21:30 +0200

    Condition txt_phone and message

--- app/src/main/java/com/example/exo2sms/MainActivity.java
+++ app/src/main/java/com/example/exo2sms/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.exo2sms;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +31,45 @@ public class MainActivity extends AppCompatActivity {
         int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS);
         if(permissionCheck== PackageManager.PERMISSION_GRANTED){
 
+            Mymessage();
+
+        }
+
+        else{
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.SEND_SMS},0 );
+        }
+    }
+
+    private void Mymessage(){
+        String phoneNumber = txt_pNumber.getText().toString().trim();
+        String Message = txt_message.getText().toString().trim();
+
+
+        if(!txt_pNumber.getText().toString().equals("") || !txt_message.getText().toString().equals("")) {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNumber, null, Message, null, null);
+
+            Toast.makeText(this, "Message Sent", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(this, "Please enter number or Message ! ", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case 0:
+                if(grantResults.length>= 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                    Mymessage();
+
+                }else{
+                    Toast.makeText(this, "You don't have required persmission ", Toast.LENGTH_SHORT).show();
+                }
+                break;
         }
     }
 }
