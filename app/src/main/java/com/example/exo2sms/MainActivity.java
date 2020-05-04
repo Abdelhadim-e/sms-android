19d6d8b1aaae2f5e9ee6e20b144b2f04701400ca
Abdelhadim-e <e.abdelhadim@gmail.com>
2020-05-04 18:31:54 +0200

    add condition sendsms

--- app/src/main/java/com/example/exo2sms/MainActivity.java
+++ app/src/main/java/com/example/exo2sms/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.exo2sms;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txt_pNumber, txt_message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        txt_message  = (EditText)findViewById(R.id.txt_message);
+        txt_pNumber = (EditText)findViewById(R.id.txt_phone_number);
+    }
+
+    public void btn_send(View view) {
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS);
+        if(permissionCheck== PackageManager.PERMISSION_GRANTED){
+
+        }
     }
 }
